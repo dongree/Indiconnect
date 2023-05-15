4216b0ad2ce986982e54448d8ed5c6878001dea3
dongree <dongwoo0307@naver.com>
2023-05-15 22:30:29 +0900

    basic setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Home2 from './pages/Home2';
+import Detail from './pages/Detail';
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/home2', element: <Home2 /> },
+  { path: '/detail', element: <Detail /> },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
