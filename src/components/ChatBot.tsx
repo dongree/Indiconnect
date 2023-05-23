648e034ed707b45576f8806891b4b106dba4df4f
dongree <dongwoo0307@naver.com>
2023-05-24 02:16:03 +0900

    navigate연결, 조금 수정

--- src/components/ChatBot.tsx
+++ src/components/ChatBot.tsx
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
-import styles from './ChatBot.module.css'
-import x from '../asset/images/x 1.png'
-import icon from '../asset/images/icon 2.png'
-import card1 from '../asset/images/card.png'
+import React, { useState } from 'react';
+import styles from './ChatBot.module.css';
+import x from '../asset/images/x 1.png';
+import icon from '../asset/images/icon 2.png';
+import card1 from '../asset/images/card.png';
+import { Link } from 'react-router-dom';
 interface Props {
-    className?: string;
-    status?: boolean;
+  className?: string;
+  status?: boolean;
 }
 
 let now = new Date();
@@ -13,56 +14,106 @@ let year = now.getFullYear(),
   month = now.getMonth(),
   day = now.getDate(),
   hour = now.getHours(),
-  minute = now.getMinutes()
+  minute = now.getMinutes();
 
-
-const ChatBot: React.FC<Props> = ({status}) => {
-const [visible, setVisible] = useState(true);
-const ChangeStatus = () => {
+const ChatBot: React.FC<Props> = ({ status }) => {
+  const [visible, setVisible] = useState(true);
+  const ChangeStatus = () => {
     setVisible(!visible);
-}
-console.log(visible)
-return (
-    <div className={styles.wrapper} style={{visibility : visible ? "visible" : "hidden"}}>
-        <div className={styles.header}>
-            <img src={x} onClick={ChangeStatus}/>
+  };
+  console.log(visible);
+  return (
+    <div
+      className={styles.wrapper}
+      style={{ visibility: visible ? 'visible' : 'hidden' }}
+    >
+      <div className={styles.header}>
+        <img src={x} onClick={ChangeStatus} />
+      </div>
+      <div className={styles.date}>
+        {year + '년 ' + month + '월 ' + day + '일 ' + hour + ':' + minute}
+      </div>
+      <div>
+        <div
+          style={{
+            marginTop: 12,
+            marginBottom: 6,
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <img src={icon} style={{ marginRight: 8, marginLeft: 16 }} />
+          <div>인디커넥츠 챗봇</div>
         </div>
-        <div className={styles.date}>
-            {year + '년 ' + month + '월 ' + day + '일 ' + hour + ':' + minute}
+        <div
+          className={styles.chatbox}
+          style={{ backgroundColor: 'white', width: 162, height: 'auto' }}
+        >
+          <div>게스트님, 반갑습니다!</div>
+          <br />
+          <div>어떤 도움이 필요하신가요?</div>
         </div>
-        <div>
-            <div style={{marginTop:12, marginBottom:6, display:"flex", alignItems:"center"}}>
-                <img src={icon} style={{marginRight:8, marginLeft:16}}/>
-                <text>인디커넥츠 챗봇</text>
-            </div>
-            <div className={styles.chatbox} style={{backgroundColor:"white", width:162, height:"auto"}}>
-                <text>게스트님, 반갑습니다!</text>
-                <br/>
-                <text>어떤 도움이 필요하신가요?</text>
-            </div>
-            <div className={styles.chatbox} style={{backgroundColor:"black", marginTop:16, width:204, height:"auto",marginLeft:160}}>
-                <text style={{color:"white"}}>홍대입구역 주변 5-6월에 진행하는 기획 공연을 찾고 싶어.</text>
-            </div>
-            <div style={{marginTop:12, marginBottom:6, display:"flex", alignItems:"center"}}>
-                <img src={icon} style={{marginRight:8, marginLeft:16}}/>
-                <text>인디커넥츠 챗봇</text>
-            </div>
-            <div className={styles.chatbox} style={{backgroundColor:"white", width:162, height:"auto"}}>
-                <text>게스트님, 반갑습니다!</text>
-                <br/>
-                <text>어떤 도움이 필요하신가요?</text>
-            </div>
-            <div style={{display:"flex"}}>
-                <img src={card1} style={{marginTop:12, marginLeft:34, width:482, height:200}} />
-            </div>
+        <div
+          className={styles.chatbox}
+          style={{
+            backgroundColor: 'black',
+            marginTop: 16,
+            width: 204,
+            height: 'auto',
+            marginLeft: 160,
+          }}
+        >
+          <div style={{ color: 'white' }}>
+            홍대입구역 주변 5-6월에 진행하는 기획 공연을 찾고 싶어.
+          </div>
         </div>
-        <div style={{backgroundColor:"white", height: 88, 
-        marginTop:40, zIndex:3, borderTop:1, borderTopStyle:"solid",
-        textAlign:"center", verticalAlign:"middle"}}>
-            <input type="text" style={{justifyContent:"center", height:41, width:342}} />
+        <div
+          style={{
+            marginTop: 12,
+            marginBottom: 6,
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <img src={icon} style={{ marginRight: 8, marginLeft: 16 }} />
+          <div>인디커넥츠 챗봇</div>
         </div>
+        <div
+          className={styles.chatbox}
+          style={{ backgroundColor: 'white', width: 162, height: 'auto' }}
+        >
+          <div>게스트님, 반갑습니다!</div>
+          <br />
+          <div>어떤 도움이 필요하신가요?</div>
+        </div>
+        <div style={{ display: 'flex' }}>
+          <Link to="/detail">
+            <img
+              src={card1}
+              style={{ marginTop: 12, marginLeft: 34, width: 482, height: 200 }}
+            />
+          </Link>
+        </div>
+      </div>
+      <div
+        style={{
+          backgroundColor: 'white',
+          height: 88,
+          marginTop: 40,
+          zIndex: 3,
+          borderTop: 1,
+          borderTopStyle: 'solid',
+          textAlign: 'center',
+          verticalAlign: 'middle',
+        }}
+      >
+        <input
+          type="text"
+          style={{ justifyContent: 'center', height: 41, width: 342 }}
+        />
+      </div>
     </div>
-);
-}
-  
-  export default ChatBot;
+  );
+};
+
+export default ChatBot;
--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,26 +1,29 @@
-import React,{useState} from "react";
-import styles from './CheckBox.module.css'
+import React, { useState } from 'react';
+import styles from './CheckBox.module.css';
 
 interface Props {
-    id?: string;
-    text?: string;
+  id?: string;
+  text?: string;
 }
-  
-const CheckBox: React.FC<Props> = ({id, text}) => {
 
+const CheckBox: React.FC<Props> = ({ id, text }) => {
   const [checked, setChecked] = useState(false); // 체크 여부 판단
   const checkHandled = () => {
     setChecked(!checked);
-    console.log()
-  }
+    console.log();
+  };
 
   return (
     <label className={styles.check_container}>
-      <input id={id} type="checkbox" checked={checked} onChange={(e) => checkHandled()} />
-      <text className={checked ? styles.text_red : styles.text}>{text}</text>
+      <input
+        id={id}
+        type="checkbox"
+        checked={checked}
+        onChange={e => checkHandled()}
+      />
+      <div className={checked ? styles.text_red : styles.text}>{text}</div>
     </label>
-  )
-}
-  
-  export default CheckBox;
-  
+  );
+};
+
+export default CheckBox;
