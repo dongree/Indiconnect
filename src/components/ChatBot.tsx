aa2fd094359254afa99bc16bdf5ed490770897e3
dongree <dongwoo0307@naver.com>
2023-05-24 11:11:14 +0900

    hojkim77 part UI 다듬기

--- src/components/ChatBot.tsx
+++ src/components/ChatBot.tsx
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 interface Props {
   className?: string;
   status?: boolean;
+  onChange: any;
 }
 
 let now = new Date();
@@ -16,101 +17,109 @@ let year = now.getFullYear(),
   hour = now.getHours(),
   minute = now.getMinutes();
 
-const ChatBot: React.FC<Props> = ({ status }) => {
+const ChatBot: React.FC<Props> = ({ onChange }) => {
   const [visible, setVisible] = useState(true);
-  const ChangeStatus = () => {
-    setVisible(!visible);
-  };
-  console.log(visible);
+  // const ()=>onChange(!visible) = () => {
+  //   setVisible(!visible);
+  // };
+  // console.log(visible);
   return (
     <div
       className={styles.wrapper}
       style={{ visibility: visible ? 'visible' : 'hidden' }}
     >
       <div className={styles.header}>
-        <img src={x} onClick={ChangeStatus} />
+        <img src={x} onClick={() => onChange(!visible)} />
       </div>
-      <div className={styles.date}>
-        {year + '년 ' + month + '월 ' + day + '일 ' + hour + ':' + minute}
-      </div>
-      <div>
-        <div
-          style={{
-            marginTop: 12,
-            marginBottom: 6,
-            display: 'flex',
-            alignItems: 'center',
-          }}
-        >
-          <img src={icon} style={{ marginRight: 8, marginLeft: 16 }} />
-          <div>인디커넥츠 챗봇</div>
+      <div className={styles.main}>
+        <div className={styles.date}>
+          {year + '년 ' + month + '월 ' + day + '일 ' + hour + ':' + minute}
         </div>
-        <div
-          className={styles.chatbox}
-          style={{ backgroundColor: 'white', width: 162, height: 'auto' }}
-        >
-          <div>게스트님, 반갑습니다!</div>
-          <br />
-          <div>어떤 도움이 필요하신가요?</div>
-        </div>
-        <div
-          className={styles.chatbox}
-          style={{
-            backgroundColor: 'black',
-            marginTop: 16,
-            width: 204,
-            height: 'auto',
-            marginLeft: 160,
-          }}
-        >
-          <div style={{ color: 'white' }}>
-            홍대입구역 주변 5-6월에 진행하는 기획 공연을 찾고 싶어.
+        <div className={styles.chat_left}>
+          <div
+            style={{
+              marginTop: 12,
+              marginBottom: 6,
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <img src={icon} style={{ marginRight: 8, marginLeft: 16 }} />
+            <div>인디커넥츠 챗봇</div>
+          </div>
+          <div
+            className={styles.chatbox}
+            style={{ backgroundColor: 'white', width: 162, height: 'auto' }}
+          >
+            <div>게스트님, 반갑습니다!</div>
+            <br />
+            <div>어떤 도움이 필요하신가요?</div>
           </div>
         </div>
-        <div
-          style={{
-            marginTop: 12,
-            marginBottom: 6,
-            display: 'flex',
-            alignItems: 'center',
-          }}
-        >
-          <img src={icon} style={{ marginRight: 8, marginLeft: 16 }} />
-          <div>인디커넥츠 챗봇</div>
+        <div className={styles.chat_right}>
+          <div
+            className={styles.chatbox}
+            style={{
+              backgroundColor: 'black',
+              marginTop: 16,
+              width: 204,
+              height: 'auto',
+              // marginLeft: 160,
+            }}
+          >
+            <div style={{ color: 'white' }}>
+              홍대입구역 주변 5-6월에 진행하는 기획 공연을 찾고 싶어.
+            </div>
+          </div>
         </div>
-        <div
-          className={styles.chatbox}
-          style={{ backgroundColor: 'white', width: 162, height: 'auto' }}
-        >
-          <div>게스트님, 반갑습니다!</div>
-          <br />
-          <div>어떤 도움이 필요하신가요?</div>
+        <div className={styles.chat_left}>
+          <div
+            style={{
+              marginTop: 12,
+              marginBottom: 6,
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <img src={icon} style={{ marginRight: 8, marginLeft: 16 }} />
+            <div>인디커넥츠 챗봇</div>
+          </div>
+          <div
+            className={styles.chatbox}
+            style={{ backgroundColor: 'white', width: 162, height: 'auto' }}
+          >
+            <div>게스트님, 반갑습니다!</div>
+            <br />
+            <div>어떤 도움이 필요하신가요?</div>
+          </div>
         </div>
-        <div style={{ display: 'flex' }}>
+
+        <div className={styles.chat_right}>
           <Link to="/detail">
             <img
               src={card1}
-              style={{ marginTop: 12, marginLeft: 34, width: 482, height: 200 }}
+              style={{
+                marginTop: 12,
+                marginLeft: 34,
+                width: 482,
+                height: 200,
+              }}
             />
           </Link>
         </div>
       </div>
+
       <div
         style={{
           backgroundColor: 'white',
-          height: 88,
-          marginTop: 40,
+          height: '10%',
           zIndex: 3,
-          borderTop: 1,
           borderTopStyle: 'solid',
           textAlign: 'center',
           verticalAlign: 'middle',
         }}
       >
-        <input
-          type="text"
-          style={{ justifyContent: 'center', height: 41, width: 342 }}
-        />
+        <input type="text" style={{ height: '100%', width: '100%' }} />
       </div>
     </div>
   );
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,86 +1,98 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 import CheckBox from './CheckBox';
-import styles from './Map.module.css'
-import mappin from '../asset/images/map-pin.png'
-import infomodal from '../asset/images/infomodal.png'
-import chatbot from '../asset/images/chat-bot.png'
+import styles from './Map.module.css';
+import mappin from '../asset/images/map-pin.png';
+import infomodal from '../asset/images/infomodal.png';
+import chatbot from '../asset/images/chat-bot.png';
 import ChatBot from './ChatBot';
 interface Props {
-    className?: string;
-    white?: boolean;
+  className?: string;
+  white?: boolean;
+  onChange: any;
 }
 
 declare global {
-    interface Window {
-      kakao: any;
-    }
+  interface Window {
+    kakao: any;
   }
-const Map: React.FC<Props> = ({className}) => {
+}
+const Map: React.FC<Props> = ({ onChange }) => {
   const [modalOpen, setModalOpen] = useState(false);
   const [visible, setVisible] = useState(false);
-  const ChangeStatus = () => {
-    setVisible(!visible)
-  }
+  // const ChangeStatus = () => {
+  //   setVisible(!visible)
+  // }
   const ModalOpenHandler = () => {
     setModalOpen(!modalOpen);
-  }
+  };
   const [markerclicked, setMarkerClicked] = useState(false);
   useEffect(() => {
-      var infowindow = new window.kakao.maps.InfoWindow({ zIndex: 1 })
+    var infowindow = new window.kakao.maps.InfoWindow({ zIndex: 1 });
 
-      const container = document.getElementById('myMap')
-      var map = new window.kakao.maps.Map(container, {
-        center: new window.kakao.maps.LatLng(38.450701, 126.570667), // 중심 좌표
-        level: 10
-      })
-      var selectedMarker: any = null;
-      var imageSize = new window.kakao.maps.Size(18, 24.5) // 마커이미지의 크기입니다
-      var imageOption = {}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-      var markerImage = new window.kakao.maps.MarkerImage(mappin, imageSize, imageOption)
-
-      function displayMarker(x: number, y: number) {
-        let marker = new window.kakao.maps.Marker({
-          map: map,
-          position: new window.kakao.maps.LatLng(x, y),
-          image : markerImage
-        })
-        marker.setMap(map);
-  
-        window.kakao.maps.event.addListener(marker, 'click', function () {
-
-          if (!selectedMarker || marker != selectedMarker){
-            !!selectedMarker && selectedMarker.setImage(markerImage);
-
-            marker.setImage(new window.kakao.maps.MarkerImage(mappin, 
-              new window.kakao.maps.Size(24.55, 31.41), 
-              imageOption))
-            //infowindow.setContent('<div style="padding:5px;font-size:12px;">' + '</div>')
-            //infowindow.open(map, marker)
-            ModalOpenHandler()
-          }
-          selectedMarker = marker;
-        })
-      }
-      displayMarker(38.450701, 126.570667);
-      displayMarker(38.4, 126.5);
-
-    }, [])
-
-    return (
-        <div style={{position:"relative"}}>
-            <div id="myMap" className={styles.map}>
-            </div>
-            <div className={styles.checkbox_container}>
-              <CheckBox text='기획 공연'/>
-              <CheckBox text='대관 공연'/>
-            </div>
-            <img className={modalOpen ? styles.chatbot_up : styles.chatbot_down} src={chatbot} onClick={ChangeStatus}/>
-            <img className={modalOpen ? styles.modal_open :styles.modal_close } src={infomodal}/>
-            {visible ? <ChatBot/> : null}
-        </div>
-
+    const container = document.getElementById('myMap');
+    var map = new window.kakao.maps.Map(container, {
+      center: new window.kakao.maps.LatLng(37.55743, 126.92696), // 중심 좌표
+      level: 5,
+    });
+    var selectedMarker: any = null;
+    var imageSize = new window.kakao.maps.Size(18, 24.5); // 마커이미지의 크기입니다
+    var imageOption = {}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+    var markerImage = new window.kakao.maps.MarkerImage(
+      mappin,
+      imageSize,
+      imageOption
     );
-  }
-  
-  export default Map;
+
+    function displayMarker(x: number, y: number) {
+      let marker = new window.kakao.maps.Marker({
+        map: map,
+        position: new window.kakao.maps.LatLng(x, y),
+        image: markerImage,
+      });
+      marker.setMap(map);
+
+      window.kakao.maps.event.addListener(marker, 'click', function () {
+        if (!selectedMarker || marker != selectedMarker) {
+          !!selectedMarker && selectedMarker.setImage(markerImage);
+
+          marker.setImage(
+            new window.kakao.maps.MarkerImage(
+              mappin,
+              new window.kakao.maps.Size(24.55, 31.41),
+              imageOption
+            )
+          );
+          //infowindow.setContent('<div style="padding:5px;font-size:12px;">' + '</div>')
+          //infowindow.open(map, marker)
+          ModalOpenHandler();
+        }
+        selectedMarker = marker;
+      });
+    }
+    displayMarker(37.55543, 126.92296);
+    displayMarker(37.55943, 126.92896);
+  }, []);
+
+  return (
+    <div style={{ position: 'relative', height: '80%' }}>
+      <div id="myMap" className={styles.map}></div>
+      <div className={styles.checkbox_container}>
+        <CheckBox text="기획 공연" />
+        <CheckBox text="대관 공연" />
+      </div>
+      <img
+        className={modalOpen ? styles.chatbot_up : styles.chatbot_down}
+        src={chatbot}
+        onClick={() => onChange(!visible)}
+      />
+      <img
+        className={modalOpen ? styles.modal_open : styles.modal_close}
+        src={infomodal}
+      />
+      {/* {visible ? <ChatBot/> : null} */}
+    </div>
+  );
+};
+
+export default Map;
